10cba180a6709506682a1657fd87bec4aeda4784
Dilpers <108380632+Dilpers@users.noreply.github.com>
2022-07-17 16:30:40 -0400

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,21 +1,63 @@
 while not game_over:
-    #Ask for player 1 input
-    if turn == 0:
-        col = int(input("Player 1, Choose a column(0-6):"))
-        #Player 1 will drop a piece on the board
-        if is_valid_location(board,col):
-            row = get_next_open_row(board,col)
-            drop_piece(board,row,col,1)
-         
-    #Ask for player 2 input
-    else:
-        col = int(input("Player 2, Choose a column(0-6):"))
-        #Player 2 will drop a piece on the board
-        if is_valid_location(board,col):
-            row = get_next_open_row(board,col)
-            drop_piece(board,row,col,2)
  
-    print_board(board)
-             
-    turnOrder += 1
-    turnOrder = turnOrder % 2
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+ 
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+            else: 
+                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+        pygame.display.update()
+ 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+            #print(event.pos)
+            # Ask for Player 1 Input
+            if turn == 0:
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARESIZE))
+ 
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 1)
+ 
+                    if winning_move(board, 1):
+                        label = myfont.render("Player 1 wins!!", 1, RED)
+                        screen.blit(label, (40,10))
+                        game_over = True
+
+	if Gamemode == "AIEasy"
+
+	else if Gamemode == "AIMedium"
+
+	else if Gamemode == "AIHard"
+
+	else
+ 
+ 
+            # # Ask for Player 2 Input
+            else:               
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARESIZE))
+ 
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 2)
+ 
+                    if winning_move(board, 2):
+                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
+                        screen.blit(label, (40,10))
+                        game_over = True
+ 
+            print_board(board)
+            draw_board(board)
+ 
+            turn += 1
+            turn = turn % 2
+ 
+            if game_over:
+                pygame.time.wait(3000)
