631014d6eb9e2c7c9daf88870b65963245d77fd7
Dilpers <108380632+Dilpers@users.noreply.github.com>
2022-07-24 19:22:18 -0400

    Update game.py

--- game.py
+++ game.py
@@ -300,10 +300,10 @@ def selection2():
             else:
                 col = random.randint(0,6)
 
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, 2)
-                    if winning_move(board, 2):
+                if is_valid(board, col):
+                    row = next_row(board, col)
+                    play_piece(board, row, col, 2)
+                    if win(board, 2):
                         label = myfont.render("Player 2 wins", 1, YELLOW)
                         screen.blit(label, (40,10))
                         game_over = True
